2451ec9fe0abf36c43ccafa316f2d0532fcb411c
peizhouyu <peizhouyu@live.com>
2019-04-09 20:24:18 +0800

    继续完善信息

--- src/main/java/cn/mrpei/controller/IndexController.java
+++ src/main/java/cn/mrpei/controller/IndexController.java
@@ -2,7 +2,7 @@ package cn.mrpei.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 /**
  * @author peizhouyu (大数据与智能供应链事业部-大数据平台部-平台产品研发部)
@@ -14,18 +14,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @copyright Copyright © 2004-2019 京东JD.com ALL Right Reserved
  * @see
  */
+
 @Controller
-@RequestMapping("/index")
 public class IndexController {
 
-    @RequestMapping("/hello")
-    public String hello(){
+    @RequestMapping("/")
+    public String index(){
         return "index";
     }
 
-    @RequestMapping("/say")
-    @ResponseBody
-    public String index(String name){
-        return "Hello" + name;
-    }
 }
